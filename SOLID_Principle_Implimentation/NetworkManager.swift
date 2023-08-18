ecc010ec42faa383b40f2c07f9865ec22c0a7200
KamsQue <kamqasim1@gmail.com>
2023-08-18 19:52:10 +0200

    Initial Commit

---
+++ SOLID_Principle_Implimentation/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  SOLID_Principle_Implimentation
+//
+//  Created by KamsQue on 18/08/2023.
+//
+
+import Foundation
---
+++ SOLID_Principle_Implimentation/OutletModel.swift
@@ -0,0 +1,8 @@
+//
+//  OutletModel.swift
+//  SOLID_Principle_Implimentation
+//
+//  Created by KamsQue on 19/08/2023.
+//
+
+import Foundation
