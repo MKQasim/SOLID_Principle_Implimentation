1f560528c33c002956d331465ff2551666fbcf02
KamsQue <kamqasim1@gmail.com>
2023-08-19 13:50:22 +0200

    [Muhammad] remaining files

--- SOLID_Principle_Implimentation/ApiManager/NetworkManager.swift
+++ SOLID_Principle_Implimentation/ApiManager/NetworkManager.swift
@@ -7,33 +7,71 @@
 
 import Foundation
 
+typealias APICompletion<Data> = (Result<Data, FoodOutletError>) -> Void
+typealias ResponseCompletion<T> = (Result<T, FoodOutletError>) -> Void
+typealias APIRequest<T> = (Result<T, FoodOutletError>) -> Void
 
-struct ApiManager {
-  func callApi<T: Codable>(urlString: String, responseType: T.Type, completion: @escaping (Outlet?, FoodOutletError?) -> Void) {
+
+class ApiManager {
+  let apiHandler: APIHandler
+  let responseHandler: ResponseHandler
+  
+  init(apiHandler: APIHandler = APIHandler(), responseHandler: ResponseHandler = ResponseHandler()) {
+    self.apiHandler = apiHandler
+    self.responseHandler = responseHandler
+  }
+  
+  func request<T: Codable>(urlString: String, completion: @escaping APIRequest<T>) {
     if let url = URL(string: urlString) {
-      let session = URLSession.shared.dataTask(with: url) { data, response, error in
-        if let error = error {
-          completion(nil, .networkError(error))
-          return
+      apiHandler.request(url: url) { result in
+        switch result {
+        case .success(let data):
+          self.responseHandler.parse(data: data, completion: completion)
+        case .failure(let error):
+          completion(.failure(error))
         }
-        
-        guard let data = data else {
-          completion(nil, .noDataError(NSError(domain: "No Data Error", code: 0, userInfo: nil)))
-          return
-        }
-        
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        do {
-          let response = try decoder.decode(Outlet.self, from: data)
-          completion(response, nil)
-        } catch {
-          completion(nil, .decodingError(NSError(domain: "Parsing Error", code: 0, userInfo: nil)))
-        }
-      }.resume()
+      }
     }
   }
 }
+
+class APIHandler {
+  func request(url: URL, completion: @escaping APICompletion<Data>) {
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      if let error = error {
+        completion(.failure(.networkError(error)))
+        return
+      }
+      
+      guard let data = data else {
+        completion(.failure(.noDataError(NSError(domain: "No Data Error", code: 0, userInfo: nil))))
+        return
+      }
+      
+      completion(.success(data))
+    }.resume()
+  }
+}
+
+class ResponseHandler {
+  func parse<T: Codable>(data: Data, completion: @escaping ResponseCompletion<T>) {
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    
+    do {
+      let decodedResponse = try decoder.decode(T.self, from: data)
+      completion(.success(decodedResponse))
+    } catch {
+      completion(.failure(.decodingError(error)))
+    }
+  }
+}
+
+
+
+
+
+
 
 
 
