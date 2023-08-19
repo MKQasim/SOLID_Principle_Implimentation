f3353c65e1f0d7f374c234989dbdf0705a08ee5e
KamsQue <kamqasim1@gmail.com>
2023-08-19 13:49:33 +0200

    [Muhammad] // steps1: Check url is working // step2 : Check response json is valid // step3 : Create Models // step4 : Create Client API Manager -> // step5 : Create Progeess Loader start stop // step6 : Api Request Handler -> T // step7 : Api Response Handler -> T // step8 : Convert completion TypAalies -> T // step9 : Error Handling -> T

--- SOLID_Principle_Implimentation/Models/OutletModel.swift
+++ SOLID_Principle_Implimentation/Models/OutletModel.swift
@@ -11,14 +11,6 @@ import Foundation
 struct Outlet: Codable {
   let page, perPage, total, totalPages: Int
   let data: [Datum]
-  
-  enum CodingKeys: String, CodingKey {
-    case page
-    case perPage
-    case total
-    case totalPages
-    case data
-  }
 }
   // MARK: - Datum
 struct Datum: Codable {
@@ -27,24 +19,12 @@ struct Datum: Codable {
   let estimatedCost: Int
   let userRating: UserRating
   let id: Int
-  
-  enum CodingKeys: String, CodingKey {
-    case city, name
-    case estimatedCost
-    case userRating
-    case id
-  }
 }
 
   // MARK: - UserRating
 struct UserRating: Codable {
   let averageRating: Double
   let votes: Int
-  
-  enum CodingKeys: String, CodingKey {
-    case averageRating
-    case votes
-  }
 }
   // MARK: - FoodOutletError
 enum FoodOutletError: Error {
@@ -52,4 +32,3 @@ enum FoodOutletError: Error {
   case decodingError(Error)
   case noDataError(Error)
 }
-
